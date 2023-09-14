260e6d0fd5a9df3c911e2cff0638dd2dabfd520d
siddhantkushwaha <k16.siddhant@gmail.com>
2023-09-14 21:44:05 +0530

    First commit

--- run_FFT_analyzer.py
+++ run_FFT_analyzer.py
@@ -1,6 +1,8 @@
 import argparse
-from src.stream_analyzer import Stream_Analyzer
 import time
+
+from src.stream_analyzer import Stream_Analyzer
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -17,6 +19,7 @@ def parse_args():
                         help='when true process sleeps between frames to reduce CPU usage (recommended for low update rates)')
     return parser.parse_args()
 
+
 def convert_window_ratio(window_ratio):
     if '/' in window_ratio:
         dividend, divisor = window_ratio.split('/')
@@ -27,31 +30,33 @@ def convert_window_ratio(window_ratio):
         return float_ratio
     raise ValueError('window_ratio should be in the format: float/float')
 
+
 def run_FFT_analyzer():
     args = parse_args()
     window_ratio = convert_window_ratio(args.window_ratio)
 
     ear = Stream_Analyzer(
-                    device = args.device,        # Pyaudio (portaudio) device index, defaults to first mic input
-                    rate   = None,               # Audio samplerate, None uses the default source settings
-                    FFT_window_size_ms  = 60,    # Window size used for the FFT transform
-                    updates_per_second  = 1000,  # How often to read the audio stream for new data
-                    smoothing_length_ms = 50,    # Apply some temporal smoothing to reduce noisy features
-                    n_frequency_bins = args.frequency_bins, # The FFT features are grouped in bins
-                    visualize = 1,               # Visualize the FFT features with PyGame
-                    verbose   = args.verbose,    # Print running statistics (latency, fps, ...)
-                    height    = args.height,     # Height, in pixels, of the visualizer window,
-                    window_ratio = window_ratio  # Float ratio of the visualizer window. e.g. 24/9
-                    )
+        device=args.device,  # Pyaudio (portaudio) device index, defaults to first mic input
+        rate=None,  # Audio samplerate, None uses the default source settings
+        FFT_window_size_ms=60,  # Window size used for the FFT transform
+        updates_per_second=1000,  # How often to read the audio stream for new data
+        smoothing_length_ms=50,  # Apply some temporal smoothing to reduce noisy features
+        n_frequency_bins=args.frequency_bins,  # The FFT features are grouped in bins
+        visualize=1,  # Visualize the FFT features with PyGame
+        verbose=args.verbose,  # Print running statistics (latency, fps, ...)
+        height=args.height,  # Height, in pixels, of the visualizer window,
+        window_ratio=window_ratio  # Float ratio of the visualizer window. e.g. 24/9
+    )
 
-    fps = 60  #How often to update the FFT features + display
+    fps = 60  # How often to update the FFT features + display
     last_update = time.time()
     while True:
-        if (time.time() - last_update) > (1./fps):
+        if (time.time() - last_update) > (1. / fps):
             last_update = time.time()
             raw_fftx, raw_fft, binned_fftx, binned_fft = ear.get_audio_features()
         elif args.sleep_between_frames:
-            time.sleep(((1./fps)-(time.time()-last_update)) * 0.99)
+            time.sleep(((1. / fps) - (time.time() - last_update)) * 0.99)
+
 
 if __name__ == '__main__':
     run_FFT_analyzer()
